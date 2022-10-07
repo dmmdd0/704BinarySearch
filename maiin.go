cec63ab57c08ee612b920bc34ef6088d144ad32d
dmmdd0 <witty.sock3113@fastmail.com>
2022-10-07 18:02:50 +0300

    done

--- maiin.go
+++ maiin.go
@@ -1,10 +1,35 @@
 package main
 
+import "fmt"
+
 func main() {
-	
+
+	nums := []int{99}
+	target := 2
+
+	fmt.Println(search(nums, target))
+
 }
 
 func search(nums []int, target int) int {
+	lower := 0
+	upper := len(nums) - 1
+	mid := 0
+	guess := 0
 
+	for lower <= upper {
+		mid = (lower + upper) / 2
+		guess = nums[mid]
+
+		if guess == target {
+			return mid
+		}
+
+		if guess > target {
+			upper = mid - 1
+		} else {
+			lower = mid + 1
+		}
+	}
+	return -1
 }
-
